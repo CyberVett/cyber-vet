102f64ef216b08e09de1cfe7653e354082949541
toyurc <adebayoige.toyosi@gmail.com>
2020-09-14 16:02:36 +0100

    Fix: updated next env ts declaration file

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,3 +2,16 @@
 /// <reference types="next/types/global" />
 
 /// <reference types="next-images" />
+
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
